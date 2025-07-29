40a6b2120da127d34f0db81d6dd55170a76550cb
JussieB <93795939+JussieB@users.noreply.github.com>
2025-07-28 20:43:07 -0500

    psuedo coded kinda ref main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+#mostly pseucode
 # import _____ 
 #insert with the import the stuff from structural and thermal analysis
+#if else statements regarding the imports of files
+#else  
+#  print out channel numbers with areas of conversion
+#also run another if else for structural analysis conversion
+#generate the plots with html for all of the data
+#save to file
