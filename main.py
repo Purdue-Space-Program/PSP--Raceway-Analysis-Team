51b127e2260c0f54c6cdfb5cda9040afce87d83d
JussieB <93795939+JussieB@users.noreply.github.com>
2025-07-30 14:28:33 -0500

    im so sorry for anyone who needs to update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import plt
 import np
 #import subplot - we need to create that 
 #local import 
+#stuff thats hard to understand pt.1
 if instance  ():
   print "file available"
   #that was very bs-ed im so sorry
@@ -22,9 +23,28 @@ else:
   file_name = input("enter name for file requested")
   #wait when was input created ;w;
   file_name = file_name + ".txt"
-  #another round of cea output that i cant understand for the love of god im so sorry for someone who has to do this
+  #another round of cea output that i cant understand for the love of god im so sorry for anyone trying to clean this up prayers to you
 
 #dimensions of the raceway will all of code around here
 
 chanl_num = 0
-print ("Channel Number: "chanl_num)
+print ("Channel Number: ", chanl_num)
+#surface_roughness = some data from strucutral analysis
+#print ("Surface Roughness:  " surface_roughness)
+#stuff thats hard to understand pt.2 :(
+
+#weird thing with non convergence function thing
+print("channels/stations did not converge")
+print ("Channel Number:  ", chanl_num)
+
+i = 0
+radius_check = r_arr[i]
+#WHEN DID THE r-arr LIST GET MADE WHAT
+radius_new = r_arr[i + 1]
+#ok lots of stuff get more complicated from here sdfsdfsf
+
+#if boolean with stuff thats hard to understand pt. 3
+#axes and phases 
+#plot code if someone wants to add some that stuff
+
+#idk how to explain literally the last 200 lines of code SJNDFDFDFJNF
