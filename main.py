6d9fa4a4babe74635b491e5bd19bd0bfecdedb96
JussieB <93795939+JussieB@users.noreply.github.com>
2025-07-30 13:20:38 -0500

    updated another referenced clearly not good main.py

--- main.py
+++ main.py
@@ -7,5 +7,24 @@
 #also run another if else for structural analysis conversion
 #generate the plots with html for all of the data
 #save to file
+
+
 import plt
 import np
+#import subplot - we need to create that 
+#local import 
+if instance  ():
+  print "file available"
+  #that was very bs-ed im so sorry
+  #cea output and file idk what those mean at all??? like i dont understand what the variables do aaaaa
+else:
+  print "file is not available"
+  file_name = input("enter name for file requested")
+  #wait when was input created ;w;
+  file_name = file_name + ".txt"
+  #another round of cea output that i cant understand for the love of god im so sorry for someone who has to do this
+
+#dimensions of the raceway will all of code around here
+
+chanl_num = 0
+print ("Channel Number: "chanl_num)
